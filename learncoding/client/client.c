c19c1196504bbcac02e557557870ac8d45aca8ff
opas <opaschen@123u.com>
2016-11-21 21:23:42 +0800

    add file

--- learncoding/client/client.c
+++ learncoding/client/client.c
@@ -6,10 +6,10 @@ int main(int argc, char *argv[])
 	char recvline[MAXLINE+1];
 	struct sockaddr_in servaddr;
 	if(argc != 2) {
-		fpringf(stderr, "usage: client <IP>\n");
+		fprintf(stderr, "usage: client <IP>\n");
 		exit(1);
 	}
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) <= 0) {
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		fprintf(stderr , "inet_pton error for %s\n", argv[1]);
 		exit(1);
 	}
@@ -18,6 +18,21 @@ int main(int argc, char *argv[])
 	servaddr.sin_port = htons(PORT);
 	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
 		fprintf(stderr, "inet_pton error for %s\n", argv[1]);
+		exit(1);
+	}
+	if(connect(sockfd,(SA *)&servaddr, sizeof(servaddr)) < 0) {
+		fprintf(stderr, "connect error\n");
+		exit(1);
+	}
+	while((n == read(sockfd, recvline, MAXLINE)) > 0) {
+		recvline[n] = 0;
+		if(fputs(recvline, stdout) == EOF) {
+			fprintf(stderr, "fputs error\n");
+			exit(1)
+		}
+	}
+	if(n < 0) {
+		fprintf(stderr, "read error\n");
 		exit(1);
 	}
 	return 0;
