10148b53996c0cd62150aef6f492e27d8491ebab
opas <opaschen@123u.com>
2017-01-06 10:05:19 +0800

    mv to Turtle

--- Turtle/unp.h
+++ Turtle/unp.h
@@ -1,15 +1,20 @@
 #ifndef __unp_h
 #define __unp_h
+#include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/un.h>
 /* define var */
-#define MAXLINE		4096
-#define IPV6
+#define MAXLINE 4096
+#define PORT 9999
+#define LISTENQ 1024
+#define SA struct sockaddr
+#define IPV6 11
 /*sock_ntop.c */
 char * sock_ntop(const struct sockaddr *sa, socklen_t salen);
 char * Sock_ntop(const struct sockaddr *sa, socklen_t salen);
