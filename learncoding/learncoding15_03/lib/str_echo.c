052632b561eedad1946de25422d2c3c1354f9815
opaser <1017370773@qq.com>
2017-06-01 19:57:38 +0800

    unix udp

---
+++ learncoding/learncoding15_03/lib/str_echo.c
@@ -0,0 +1,23 @@
+#include "unp.h"
+
+void str_echo(int sockfd)
+{
+	ssize_t		n;
+	char			buf[MAXLINE];
+again:
+	while(( n = read(sockfd, buf, MAXLINE)) > 0 ) {
+		printf("Uinx tcp receive length => %d, detail => %s", n, buf);
+		buf[n-1] = '.';
+		buf[n] = 0;
+		strcat(buf,"chen xin I love you too! use Uinx tcp");
+		n = strlen(buf);
+		buf[n] = '\n';
+		n = n + 1;
+		printf("tcp send length => %d, detail => %s", n, buf);
+		Writen(sockfd, buf, n);
+	}
+	if(n < 0 && errno == EINTR)
+		goto again;
+	else if (n < 0)
+		err_sys("str_echo: read error");
+}
